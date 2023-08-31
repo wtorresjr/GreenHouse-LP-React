1e455a507461df705dc0489ce198ad655318b43b
Will Torres <w.torresjr@gmail.com>
2023-08-31 00:23:34 -0400

    Phase 5 completed

--- src/components/Hygrometer/Hygrometer.js
+++ src/components/Hygrometer/Hygrometer.js
@@ -1,14 +1,19 @@
 import ReactSlider from "react-slider";
 import "./Hygrometer.css";
+import { ClimateContext, useClimate } from "../../context/ClimateContext";
+import { useContext } from "react";
 
 function Hygrometer() {
+  const { humidity, setHumidity } = useContext(ClimateContext);
   return (
     <section>
       <h2>Hygrometer</h2>
-      <div className="actual-humid">Actual Humidity: {"y"}%</div>
+      <div className="actual-humid">Actual Humidity: {humidity}%</div>
       <ReactSlider
-        value={10}
-        onAfterChange={(val) => {}}
+        value={humidity}
+        onAfterChange={(val) => {
+          setHumidity(val);
+        }}
         className="hygrometer-slider"
         thumbClassName="hygrometer-thumb"
         trackClassName="hygrometer-track"
--- src/context/ClimateContext.js
+++ src/context/ClimateContext.js
@@ -1,19 +1,20 @@
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState } from "react";
 
 export const ClimateContext = createContext();
 
-export const useClimate = () => useContext(ClimateContext)
+export const useClimate = () => useContext(ClimateContext);
 
- const ClimateProvider = (props) => {
-    const [temp, setTemp] = useState(50);
+const ClimateProvider = (props) => {
+  const [temp, setTemp] = useState(50);
+  const [humidity, setHumidity] = useState(40);
 
-    return (
-        <ClimateContext.Provider value={{ temp, setTemp }}>
-            {props.children}
-        </ClimateContext.Provider >
-    )
-}
+  return (
+    <ClimateContext.Provider value={{ temp, setTemp, humidity, setHumidity }}>
+      {props.children}
+    </ClimateContext.Provider>
+  );
+};
 
 export default ClimateProvider;
